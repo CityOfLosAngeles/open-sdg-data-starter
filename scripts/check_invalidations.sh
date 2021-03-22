8c520be801d1af9d6732924bddd68245bf75a38f
Timothy <timothy.margono@lacity.org>
2021-03-22 11:46:27 -0700

    Add comments to script
    close #14

--- scripts/check_invalidations.sh
+++ scripts/check_invalidations.sh
@@ -7,12 +7,12 @@
 
 # Functions
 
-# Check Command
+# Check Command Exists
 function checkCommand {
 	type -P $1 &>/dev/null || fail "Unable to find $1, please install it and run this script again."
 }
 
-# Completed
+# Completed Script
 function scriptExit(){
 	HorizontalRule
 	echo "Exiting Script"
@@ -21,23 +21,25 @@ function scriptExit(){
 	exit 0
 }
 
-# Fail
+# Script Failed
 function fail(){
 	echo "Failure: $*"
 	exit 1
 }
 
-# Horizontal Rule
+# Horizontal Rule, print divider line
 function HorizontalRule(){
 	echo "============================================================"
 }
 
-# List Invalidations
+# List Cloudfront Invalidations
 function listInvalidations(){
 	HorizontalRule
 	echo "Checking for Invalidations In Progress..."
 
+	# jq used to parse aws json output
 	invalidations=$(aws cloudfront list-invalidations --distribution-id $CLOUDFRONT_DIST_ID | jq '.InvalidationList | .Items | .[] | select(.Status != "Completed") | .Id' | cut -d \" -f2)
+	# Check if invalidation exists, if so output status
 	if ! [ -z "$invalidations" ]; then
 		HorizontalRule
 		echo "Invalidation in progress: $invalidations"
@@ -45,8 +47,9 @@ function listInvalidations(){
 	fi
 }
 
-# Check the Invalidation Status
+# Check the Cloudfront Invalidation Status
 function checkInvalidationstatus(){
+	# Check if invalidation exists, if not exit script
 	if [ -z "$invalidations" ]; then
 		echo No CloudFront Invalidations In Progress.
 		HorizontalRule
@@ -54,8 +57,10 @@ function checkInvalidationstatus(){
 	else
 		while IFS= read -r invalidationid
 		do
+			# jq used to parse aws json output
 			invalidationStatus=$(aws cloudfront get-invalidation --distribution-id $CLOUDFRONT_DIST_ID --id $invalidationid | jq '.Invalidation | .Status' | cut -d \" -f2)
 
+			# While invalidation in progress, sleep for 10 seconds and check again
 			while [ $invalidationStatus = "InProgress" ]; do
 				HorizontalRule
 				echo "Invalidation ID: $invalidationid" 
@@ -66,6 +71,7 @@ function checkInvalidationstatus(){
 				checkInvalidationstatus
 			done
 
+			# If invalidation is completed, exit the script
 			if [ $invalidationStatus = "Completed" ]; then
 				echo "CloudFront Invalidation $invalidationStatus"
 				scriptExit
@@ -74,6 +80,9 @@ function checkInvalidationstatus(){
 	fi
 }
 
+# Make sure jq exists on the system
 checkCommand "jq"
+# Grab all invalidations
 listInvalidations
+# Make sure invalidations are completed
 checkInvalidationstatus
